47071d1f81e2f7e8fd3d654a03415fa782175c3d
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2020-07-13 16:09:22 -0700

    add JSX and CSS to TrackList + import to rel comps

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TrackList extends React.Component {
+    render() {
+        return (
+            <div className='TrackList'>
+                {/* map method will go here */}
+            </div>
+        )
+    }
+}
+
+export default TrackList;
