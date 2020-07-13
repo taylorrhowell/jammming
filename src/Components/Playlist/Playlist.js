537b0ed8b8851d2965403d4894fccddc6e797963
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2020-07-13 16:17:46 -0700

    update imports on Playlist/TrackList, create Track

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -6,8 +6,8 @@ class Playlist extends React.Component {
     render() {
         return (
             <div className='Playlist'>
-                <input value='New Playlist'/>
-                <TrackList />
+                <input defaultValue={'New Playlist'}/>
+                {/* <TrackList /> */}
                 <button className='Playlist-save'>SAVE TO SPOTIFY</button>
             </div>
         )
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './TrackList.css';
+import Track from '../Track/Track';
 
 class TrackList extends React.Component {
     render() {
