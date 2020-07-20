c11c8c3c7aa81e87d5a211ef2da8e232e2e86bb1
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2020-07-20 13:46:22 -0700

    add savePlaylist method, pass to Playlist comp

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -40,6 +40,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track) {
@@ -60,6 +61,11 @@ class App extends React.Component {
     this.setState({playlistName: name});
   }
 
+  savePlaylist() {
+    let trackURIs = this.state.playlistTracks.map(track => track.id)
+    return trackURIs;
+  }
+
   render() {
     return (
     <div>
@@ -68,7 +74,7 @@ class App extends React.Component {
         <SearchBar />
         <div className='App-playlist'>
           <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
-          <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}/>
+          <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist}/>
         </div>
       </div>
     </div>
